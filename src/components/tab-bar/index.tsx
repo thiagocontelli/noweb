1b80a4a50cac9d2fe53e322eed5bd7e11c3e9a5e
Thiago Contelli <thiagocontellid@gmail.com>
2023-10-18 21:33:25 -0300

    feat: create TabBar component

---
+++ src/components/tab-bar/index.tsx
@@ -0,0 +1,78 @@
+import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Button, HStack, useTheme } from 'native-base';
+import { TouchableOpacity, View } from 'react-native';
+
+type Props = BottomTabBarProps
+
+export function TabBar({ descriptors, navigation, state }: Props) {
+  const theme = useTheme()
+
+  return (
+    <View style={{ backgroundColor: 'transparent', position: 'absolute', left: 0, bottom: 0, right: 0 }}>
+      <LinearGradient colors={['transparent', 'white', 'white', 'white']}>
+        <HStack mb={6} mt={8} mx={4} justifyContent={'space-between'} alignItems={'center'}>
+          {state.routes.map((route, index) => {
+            const { options } = descriptors[route.key]
+
+            const isFocused = state.index === index
+
+            function onPress() {
+              const event = navigation.emit({
+                type: 'tabPress',
+                target: route.key,
+                canPreventDefault: true,
+              });
+              if (!isFocused && !event.defaultPrevented) {
+                navigation.navigate(route.name, { merge: true });
+              }
+            };
+
+            function onLongPress() {
+              navigation.emit({
+                type: 'tabLongPress',
+                target: route.key,
+              });
+            };
+
+            function getIcon(color: string = isFocused ? theme.colors.primary[600] : theme.colors.gray[400], size: number = 32) {
+              return options.tabBarIcon && options.tabBarIcon({ color, focused: isFocused, size })
+            }
+
+            return (
+              <>
+                {index === 2 ? (
+                  <Button
+                    onPress={onPress}
+                    onLongPress={onLongPress}
+                    borderRadius={24}
+                    height={16}
+                    width={16}
+                    background={isFocused ? 'primary.800' : undefined}
+                  >
+                    {getIcon('white', 40)}
+                  </Button>
+                ) : (
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: isFocused ? theme.colors.primary[50] : undefined,
+                      padding: 10,
+                      borderRadius: 16
+                    }}
+                    onPress={onPress}
+                    onLongPress={onLongPress}
+                    accessibilityRole="button"
+                    accessibilityState={isFocused ? { selected: true } : {}}
+                    accessibilityLabel={options.tabBarAccessibilityLabel}
+                  >
+                    {getIcon()}
+                  </TouchableOpacity>
+                )}
+              </>
+            )
+          })}
+        </HStack>
+      </LinearGradient>
+    </View>
+  );
+}
